16b63c0e9e9dd259e668a509f1b0a47671b48800
dominic <dominiccordeiro@gmail.com>
2018-06-17 22:41:57 +0800

    Can now hold keys to move them

--- Rpi_car.py
+++ Rpi_car.py
@@ -14,13 +14,13 @@ def key_input(event):
 
     if key_press.lower() == 'w':
         arduino.write('w')
-    elif key_press.lower() == 'x':
+    elif key_press.lower() == 's':
         arduino.write('x')
     elif key_press.lower() == 'a':
         arduino.write('a')
     elif key_press.lower() == 'd':
         arduino.write('d')
-    elif key_press.lower() == 's':
+    elif key_press.lower() == 'x':
 	arduino.write('s')
     elif key_press.lower() == 'i':
 	arduino.write('i')
@@ -28,9 +28,9 @@ def key_input(event):
 	arduino.write('j')
     elif key_press.lower() == 'l':
 	arduino.write('l')
-    elif key_press.lower() == 'k':
-	arduino.write('k')
     elif key_press.lower() == ',':
+	arduino.write('k')
+    elif key_press.lower() == 'k':
 	arduino.write(',')
     elif key_press.lower() == 'o':
 	arduino.write('o')
@@ -39,7 +39,14 @@ def key_input(event):
 
     time.sleep(0.03)
 
+def brake(event):
+    # so that car stops when we stop pressing w, a, s, d 
+    arduino.write('s')
+    # so that servo stops when we release the keys
+    arduino.write('k')
+
 command = tk.Tk()
 command.bind('<KeyPress>', key_input)
+command.bind('<KeyRelease>', brake)
 command.mainloop()
 
